c2e953df3753087c0bc0324e9513065bf440cb93
Valerio [Fisso] <valerio.taralli2001@gmail.com>
2025-11-09 01:16:11 +0100

    libGUI.py

--- Codice/main.py
+++ Codice/main.py
@@ -6,65 +6,71 @@ import libExtractData as libED
 importlib.reload(libED)
 import libMASKineticTheory as libKT
 importlib.reload(libKT)
+import libGUI
+importlib.reload(libGUI)
 
 import numpy as np
 
 
-
-### Network section ###
-
-# zipPath = "../Dati/matriciPendolarismo1991.zip"
-# libED.ExtractAdjacencyMatrices(zipPath)
-
-A, W = libED.ReadAdjacencyMatrices(
-    "../Dati/AdjacencyMatricesIt91.zip",
-    "20AdjacencyMatrixSardegna.txt",
-    "20WeightedAdjacencyMatrixSardegna.txt"
-)
-Nn = A.shape[0] # Number of nodes
-
-# Degrees
-di = np.sum(A,axis=0) # Vectors of degrees
-dk, Nk = np.unique(di,return_counts=True)
-# Unique degrees and corresponding frequencies
-# Pk = counts/N
-
-# [Nonzero] Weights
-wi = W[W>0]
-wk, wNk = np.unique(wi,return_counts=True)
-# Unique weights and corresponding frequencies
-
-# Strenghts
-si = np.sum(W,axis=0)
-sk, sNk = np.unique(si,return_counts=True)
-# Unique strenghts and corresponding frequencies
-
-# libAN.DegreeDistributionFig(di,Nn)
-# libAN.WeightDistributionFig(wi)
-# libAN.StrengthDistributionFig(si)
-
-# libAN.BetweennessCentralityFig(A,di)
-# libAN.StrengthFromDegreeFig(si,di,dk,Nk)
-
-# Ck = libAN.ClusteringCoefficientFig(A,di,dk,Nk)
-# libAN.WeightedClusteringCoefficientFig(A,W,si,di,dk,Nk,Ck)
-
-# knn = libAN.AssortativityFig(A,dk)
-# libAN.WeightedAssortativityFig(W,dk,knn)
-
-# plt.show()
-# mpld3.show()
+### GUI ###
+prm = libGUI.GUI() # Parameters
 
 
+if prm['runState']:
 
-### Kinetic section ###
+    ### Network section ###
 
-popSard = 1600000 # Sardinia population in 1991
+    if prm['extData']:
+        zipPath = "../Dati/matriciPendolarismo1991.zip"
+        libED.ExtractAdjacencyMatrices(zipPath)
 
-dt = 1e-2; Nt = int(2e4)
-l  = .75; a = 1
-sigma = 5e-2
+    A, W = libED.ReadAdjacencyMatrices(
+        "../Dati/AdjacencyMatricesIt91.zip",
+        "20AdjacencyMatrixSardegna.txt",
+        "20WeightedAdjacencyMatrixSardegna.txt"
+    )
+    Nn = A.shape[0] # Number of nodes
 
-stateCities = libKT.MonteCarlo(popSard,Nn,A,Nt,dt,l,a,sigma)
-libKT.CityDistributionFig(stateCities.vtxState,Nn)
-libKT.CityAverageFig(stateCities.avgState,Nt,dt)
+    # Degrees
+    di = np.sum(A,axis=0) # Vectors of degrees
+    dk, Nk = np.unique(di,return_counts=True)
+    # Unique degrees and corresponding frequencies
+    # Pk = counts/N
+
+    # [Nonzero] Weights
+    wi = W[W>0]
+    wk, wNk = np.unique(wi,return_counts=True)
+    # Unique weights and corresponding frequencies
+
+    # Strenghts
+    si = np.sum(W,axis=0)
+    sk, sNk = np.unique(si,return_counts=True)
+    # Unique strenghts and corresponding frequencies
+
+    if prm['extData']:
+        libAN.DegreeDistributionFig(di,Nn)
+        libAN.WeightDistributionFig(wi)
+        libAN.StrengthDistributionFig(si)
+
+        libAN.BetweennessCentralityFig(A,di)
+        libAN.StrengthFromDegreeFig(si,di,dk,Nk)
+
+        Ck = libAN.ClusteringCoefficientFig(A,di,dk,Nk)
+        libAN.WeightedClusteringCoefficientFig(A,W,si,di,dk,Nk,Ck)
+
+        knn = libAN.AssortativityFig(A,dk)
+        libAN.WeightedAssortativityFig(W,dk,knn)
+
+        # plt.show()
+        # mpld3.show()
+
+
+    ### Kinetic section ###
+
+    stateCities = libKT.MonteCarlo(
+        prm['sMax'],Nn,A,
+        prm['Nt'],prm['dt'],
+        prm['l'],prm['a'],prm['sigma']
+    )
+    libKT.CityDistributionFig(stateCities.vtxState,Nn)
+    libKT.CityAverageFig(stateCities.avgState,prm['Nt'],prm['dt'])
